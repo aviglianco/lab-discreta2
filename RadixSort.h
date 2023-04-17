796affa47be48171c35328767d54aff5890b0a43
aviglianco <aviglianco22@gmail.com>
2023-04-17 12:35:16 -0300

    Añadir comentarios

--- RadixSort.h
+++ RadixSort.h
@@ -4,7 +4,8 @@
 #include "APIG23.h"
 
 /**
- * @brief Aplica el algoritmo de ordenamiento radix sort para ordenar de forma decreciente.
+ * @brief el algoritmo de ordenamiento radix sort para ordenar de forma decreciente.
+ * La complejidad temporal es O(n).
  * @param arr Arreglo de enteros a ordenar.
  * @param n Tamaño del arreglo.
  * @returns El arreglo ordenado en forma descendente.
--- main.c
+++ main.c
@@ -22,7 +22,60 @@ int main(void){
     printf("Nro. de Vértices %d\n",g->n);  
     printf("Nro. de Lados %d\n"   ,g->m);
     printf("Delta  %d\n",g->delta);
+
+    u32 *orden = calloc(g->n, sizeof(u32));
+    u32 *color = calloc(g->n, sizeof(u32));
+
+    for (u32 i = 0; i < g->n; i++){
+        orden[i] = i;
+    }
+
+    u32 cantColores = Greedy(g, orden, color);
     
+    printf("Cantidad de colores: %d\n", cantColores);
+    // Print the orden array
+    printf("Orden: ");
+    for (u32 i = 0; i < g->n; i++){
+        printf("%d ", orden[i]);
+    }
+    printf("\n");
+
+    // Print the color array
+    printf("Color: ");
+    for (u32 i = 0; i < g->n; i++){
+        printf("%d ", color[i]);
+    }
+    printf("\n");
+
+    char check = OrdenImparPar(g->n, orden, color);
+
+    // print the orden array
+    printf("Orden Impar-Par: ");
+    for (u32 i = 0; i < g->n; i++){
+        printf("%d ", orden[i]);
+    }
+    printf("\n");
+
+    printf("------------------------\n");
+    for (unsigned int i = 0; i < 3; i++){
+        printf("Ejecución: %d\n", i+1);
+        cantColores = Greedy(g, orden, color);
+        check = OrdenImparPar(g->n, orden, color);
+
+        // print the color array
+        printf("Color: ");
+        for (u32 i = 0; i < g->n; i++){
+            printf("%d ", color[i]);
+        }
+        printf("\n");
+
+        // print the orden array
+        printf("Orden Impar-Par: ");
+        for (u32 i = 0; i < g->n; i++){
+            printf("%d ", orden[i]);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
